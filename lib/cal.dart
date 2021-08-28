a2f47ead0c3fd7c652ace3e3cad64679fe7999f1
Mustafa <almasood91@gmail.com>
2021-08-28 12:05:30 +0500

    build the logic of delete function

---
+++ lib/cal.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Cal extends StatefulWidget {
+  @override
+  _CalState createState() => _CalState();
+}
+
+class _CalState extends State<Cal> {
+  var output = " ";
+  Widget xyz(no) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          output = output + no;
+        });
+      },
+      child: Text(no),
+      style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(20.0),
+          textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    );
+  }
+
+  clear() {
+    setState(() {
+      output = " ";
+    });
+  }
+
+  delete() {
+    setState(() {
+      output = output.substring(0, output.length - 1);
+    });
+  }
+
+  result() {
+    Parser p = Parser();
+    Expression exp = p.parse(output);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    setState(() {
+      output = eval.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 10, left: 10),
+                height: 100,
+                width: 380,
+                color: Colors.white,
+                child: Text(output,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold)),
+              ),
+              // 2nd Container
+              Container(
+                  height: 550,
+                  width: 380,
+                  color: Colors.white,
+                  // Column
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      // Row 1
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: clear,
+                            child: Text("C"),
+                            style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.all(20.0),
+                                textStyle: TextStyle(
+                                    fontSize: 30, fontWeight: FontWeight.bold)),
+                          ),
+                          xyz("."),
+                          xyz(","),
+                          ElevatedButton(
+                            onPressed: delete,
+                            child: Text("D"),
+                            style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.all(20.0),
+                                textStyle: TextStyle(
+                                    fontSize: 30, fontWeight: FontWeight.bold)),
+                          ),
+                        ],
+                      ),
+                      // Row 2
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          xyz("7"),
+                          xyz("8"),
+                          xyz("9"),
+                          xyz("+"),
+                        ],
+                      ),
+                      // Row 3
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          xyz("4"),
+                          xyz("5"),
+                          xyz("6"),
+                          xyz("-"),
+                        ],
+                      ),
+                      // Row 4
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          xyz("1"),
+                          xyz("2"),
+                          xyz("3"),
+                          xyz("*"),
+                        ],
+                      ),
+
+                      // Row 5
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          xyz("."),
+                          xyz("0"),
+                          xyz("/"),
+                          ElevatedButton(
+                            onPressed: result,
+                            child: Text("="),
+                            style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.all(20.0),
+                                textStyle: TextStyle(
+                                  fontSize: 30,
+                                )),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            ],
+          ),
+        ));
+  }
+}
